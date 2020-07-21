ed42d88505b2aff7d311fa8049b84bf08326ca2a
Mbaye babacar sarr <mbayesarr.baba@gmail.com>
2020-07-21 10:20:15 +0000

    Initial commit

---
+++ src/main/java/net/mbs/moduletiers/ModuleTiersApplication.java
@@ -0,0 +1,13 @@
+package net.mbs.moduletiers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleTiersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleTiersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/mbs/moduletiers/ModuleTiersApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mbs.moduletiers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleTiersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
